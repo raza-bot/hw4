0d642126c124204b93fd18c1ac5bb88d3d6a1cbe
Raza Ghulam <ahmad.abdali001@gmail.com>
2020-11-17 14:14:35 -0800

    requirement added to index.php

--- jigsaw/index.php
+++ jigsaw/index.php
@@ -2,10 +2,8 @@
 
 namespace sjsu_174\hw4; 
 
-use seekquarry\yioop\configs as SYC;
-use seekquarry\yioop\library as SYL;
-use seekquarry\yioop\library\PhraseParser;
+use monolog as SYC;
 
 require_once "vendor/autoload.php";
-print_r(PhraseParser::stemTerms("jumping", 'en-US'));
-echo SYL\crawlHash(SYC\NAME_SERVER . "1234");
+
+echo "started successfully"; 
